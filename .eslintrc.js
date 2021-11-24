001b258f76d6d4b2dc7565d5f5eba620a42e8d2f
jeereq <mingandajeereq@gmail.com>
2021-11-24 16:45:33 +0100

    feat : first commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 13,
+	},
+	rules: {
+		'no-undef': 'warn',
+		'no-unused-vars': 'warn',
+	},
+};
